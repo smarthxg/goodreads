3fe63c047704fa83e97292e59111f9770002ef56
Smarth Gupta <34389844+smarthxg@users.noreply.github.com>
2020-07-12 19:24:28 +0530

    Update user.py

--- goodreads/user.py
+++ goodreads/user.py
@@ -79,7 +79,7 @@ class GoodreadsUser():
         """Get all books and reviews on user's shelves"""
         resp = self._client.session.get("/review/list.xml",
                                         {'v': 2, 'id': self.gid, 'page': page})
-        return [review.GoodreadsReview(r) for r in resp['reviews']['review']]
+        return [r for r in resp['reviews']['review']]
 
     def shelves(self, page=1):
         """Get the user's shelves. This method gets shelves only for users with
